37d97dd01ea2da4d412b80f144a20871b941a9f2
nunsinee <71260513+nunsinee@users.noreply.github.com>
2021-06-03 21:01:37 +0200

    test landing page

--- js/video-bg.js
+++ js/video-bg.js
@@ -1,5 +1,9 @@
-const video = document.querySelector("#video");
-window.addEventListener("load", () => {
-    video.muted = true;
-    video.play();
-});
+// const video = document.querySelector("#video");
+// window.addEventListener("load", () => {
+//     video.muted = true;
+//     video.play();
+// });
+
+var mobilevideo = document.getElementsByTagName("video")[0];
+mobilevideo.setAttribute("playsinline", "");
+mobilevideo.setAttribute("muted", "");
