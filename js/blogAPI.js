3af75be1e7fd56362753973d6265a19906c1b1be
nunsinee <71260513+nunsinee@users.noreply.github.com>
2021-06-04 13:50:40 +0200

    update console.log and css

--- js/blogAPI.js
+++ js/blogAPI.js
@@ -1,11 +1,11 @@
 const urlTenBlogs =
-	"https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/posts/?per_page=6&offset=0";
+    "https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/posts/?per_page=6&offset=0";
 
 const urlMoreBlogs =
-	"https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/posts/?per_page=6&offset=6";
+    "https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/posts/?per_page=6&offset=6";
 
 const urlAuthor =
-	"https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/users/1";
+    "https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/users/1";
 
 const showPostItems = document.querySelector(".postItems-container");
 const showPostItems2 = document.querySelector(".postItems-container-more");
@@ -16,72 +16,72 @@ loading.classList.add("loader");
 
 // Fetch Api
 setTimeout(() => {
-	async function getPostItems() {
-		try {
-			// get first 6 blogs
-			const responseTenBlogs = await fetch(urlTenBlogs);
-			const getTenBlogs = await responseTenBlogs.json();
-			console.log(getTenBlogs);
+    async function getPostItems() {
+        try {
+            // get first 6 blogs
+            const responseTenBlogs = await fetch(urlTenBlogs);
+            const getTenBlogs = await responseTenBlogs.json();
 
-			// get More 6 blogs
-			const responseMoreBlogs = await fetch(urlMoreBlogs);
-			const getMoreBlogs = await responseMoreBlogs.json();
-			console.log(getMoreBlogs);
 
-			//get Author
-			const responseAuthor = await fetch(urlAuthor);
-			const getAuthor = await responseAuthor.json();
+            // get More 6 blogs
+            const responseMoreBlogs = await fetch(urlMoreBlogs);
+            const getMoreBlogs = await responseMoreBlogs.json();
 
-			//slide-content
-			showPostItems.innerHTML = "";
-			showPostItems2.innerHTML = "";
-			createPostItems(getTenBlogs, getAuthor);
-			//createPostItemsMore(getMoreBlogs, getAuthor);
 
-			const viewMore = document.querySelector("#viewMore");
-			const viewLess = document.querySelector("#viewLess");
+            //get Author
+            const responseAuthor = await fetch(urlAuthor);
+            const getAuthor = await responseAuthor.json();
 
-			viewMore.addEventListener("click", () => {
-				//createPostItemsMore(getMoreBlogs, getAuthor);
-				// showPostItems2.style.display = "block";
-				createPostItemsMore(getMoreBlogs, getAuthor);
-				viewMore.style.display = "none";
-				viewLess.style.display = "block";
-			});
+            //slide-content
+            showPostItems.innerHTML = "";
+            showPostItems2.innerHTML = "";
+            createPostItems(getTenBlogs, getAuthor);
+            //createPostItemsMore(getMoreBlogs, getAuthor);
 
-			viewLess.addEventListener("click", () => {
-				showPostItems2.innerHTML = "";
-				viewMore.style.display = "block";
-				viewLess.style.display = "none";
-			});
-		} catch (error) {
-			console.log("An error occurred");
-			showPostItems.innerHTML += `<div class="api-error"><h1>An error occurred when calling the API</h1></div>`;
-		}
-	}
-	getPostItems();
+            const viewMore = document.querySelector("#viewMore");
+            const viewLess = document.querySelector("#viewLess");
 
-	function createPostItems(getTenBlogs, authorName) {
-		for (let i = 0; i < getTenBlogs.length; i++) {
-			showPostItems.innerHTML += `
+            viewMore.addEventListener("click", () => {
+                //createPostItemsMore(getMoreBlogs, getAuthor);
+                // showPostItems2.style.display = "block";
+                createPostItemsMore(getMoreBlogs, getAuthor);
+                viewMore.style.display = "none";
+                viewLess.style.display = "block";
+            });
+
+            viewLess.addEventListener("click", () => {
+                showPostItems2.innerHTML = "";
+                viewMore.style.display = "block";
+                viewLess.style.display = "none";
+            });
+        } catch (error) {
+            console.log("An error occurred");
+            showPostItems.innerHTML += `<div class="api-error"><h1>An error occurred when calling the API</h1></div>`;
+        }
+    }
+    getPostItems();
+
+    function createPostItems(getTenBlogs, authorName) {
+        for (let i = 0; i < getTenBlogs.length; i++) {
+            showPostItems.innerHTML += `
 		<div class="post-item">
 		<div class="p-pic"><img id="smallImg" src="${getTenBlogs[i].fimg_url}" alt="${getTenBlogs[i].title.rendered}"></div>
 		<div class="p-name">${getTenBlogs[i].title.rendered}</div>
 		<div class="p-by"><span>By ${authorName.name}-${getTenBlogs[i].formatted_date}</span></div>
 		<div class="p-view"><a href="blog_detail.html?id=${getTenBlogs[i].id}">Read</a></div>`;
-		}
-	}
+        }
+    }
 
-	function createPostItemsMore(getMoreBlogs, authorName) {
-		for (let i = 0; i < getMoreBlogs.length; i++) {
-			showPostItems2.innerHTML += `
+    function createPostItemsMore(getMoreBlogs, authorName) {
+        for (let i = 0; i < getMoreBlogs.length; i++) {
+            showPostItems2.innerHTML += `
 		<div class="post-item">
 		<div class="p-pic"><img id="smallImg" src="${getMoreBlogs[i].fimg_url}" alt="${getMoreBlogs[i].title.rendered}"}"></div>
 		<div class="p-name">${getMoreBlogs[i].title.rendered}</div>
 		<div class="p-by"><span>By ${authorName.name}-${getMoreBlogs[i].formatted_date}</span></div>
 		<div class="p-view"><a href="blog_detail.html?id=${getMoreBlogs[i].id}">Read</a></div>`;
-		}
-	}
+        }
+    }
 
-	loading.classList.remove("loader");
+    loading.classList.remove("loader");
 }, 3000);
--- js/search.js
+++ js/search.js
@@ -1,41 +1,41 @@
 const search = document.querySelector("#search");
 const showSearchResult = document.querySelector("#match-list");
 
-const searchPosts = async (searchText) => {
-	const res = await fetch(
-		"https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/posts/"
-	);
-	const posts = await res.json();
+const searchPosts = async(searchText) => {
+    const res = await fetch(
+        "https://infokrabi.thaifolkinnorway.com/wp-json/wp/v2/posts/"
+    );
+    const posts = await res.json();
 
-	// Get match to current text input
-	let matches = posts.filter((post) => {
-		const regex = new RegExp(`^${searchText}`, "gi");
-		return post.title.rendered.match(regex) || post.slug.match(regex);
-	});
+    // Get match to current text input
+    let matches = posts.filter((post) => {
+        const regex = new RegExp(`^${searchText}`, "gi");
+        return post.title.rendered.match(regex) || post.slug.match(regex);
+    });
 
-	if (searchText.length === 0) {
-		matches = [];
-		showSearchResult.innerHTML = "";
-	}
-	outputHTML(matches);
+    if (searchText.length === 0) {
+        matches = [];
+        showSearchResult.innerHTML = "";
+    }
+    outputHTML(matches);
 };
 
 // Show result in HTML
 const outputHTML = (matches) => {
-	if (matches.length > 0) {
-		const html = matches
-			.map(
-				(match) => `
+    if (matches.length > 0) {
+        const html = matches
+            .map(
+                (match) => `
 				<div class="post-item-results">
 				<div class="p-pic-s"><img src="${match.images.medium}" alt="${match.title.rendered}"}></div>
 				<div class="p-name-s">${match.title.rendered}</div>
 				<div class="p-by-s"><span>By Nunsinee-${match.formatted_date}</span></div>
 				<div class="p-view-s"><a href="blog_detail.html?id=${match.id}">Read</a></div>
 				</div>`
-			)
-			.join("");
-		showSearchResult.innerHTML = html;
-	}
+            )
+            .join("");
+        showSearchResult.innerHTML = html;
+    }
 };
 
 //Search text and filter
